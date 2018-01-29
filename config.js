1c93c934c50648ea86577a5a24134c59c303e520
Alex Ramsdell <alexramsdell@gmail.com>
2018-01-29 13:55:51 -0800

    add travis yml for ci

---
+++ config.js
@@ -0,0 +1,12 @@
+const masterConfig = {
+    pinned: [
+        // list of appIds pinned to main page 
+    ]
+};
+
+// one per app
+const appConfig = {
+    appId: 'NOTASK', 
+    version: '1.0.0',
+    urlPrefix: 'notask',  // notask.edibleinterfaces.com
+};
